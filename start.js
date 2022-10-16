e16d206ca2e1363d1a5a34d324b4399f34904e97
Nazar Hotko <nazar.hotko@outlook.com>
2022-10-16 20:10:28 +0200

    first commit

---
+++ start.js
@@ -0,0 +1,20 @@
+function howOld(age,year) {
+    let currentYear = 2022;
+    let birthYear = currentYear - age;
+    if (year > currentYear) {
+      let yearDifference = year - currentYear;
+      let plusAge = age + yearDifference;
+      return `You will be ${plusAge} in the year ${year}`
+    } else if (year < currentYear){
+      let beforeBirth = birthYear - year;
+      let afterBirth = year - birthYear
+        if (birthYear > year) {
+          return `The year ${year} was ${beforeBirth} years before you were born`
+        } else if (birthYear < year) {
+          return `You were ${afterBirth} in the year ${year}`
+        }
+    }
+  }
+
+  console.log(howOld(24, 2066))
+  
